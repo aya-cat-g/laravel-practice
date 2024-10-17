cfbe88c119b8809e91b7a5f64b1884e04ca9fa7e
坂本絢華 <aya.cat.g@gmail.com>
2024-10-17 10:29:32 +0900

    ルーティング周りのテスト

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Person;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Route::model('person', Person::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\HelloController;
+use App\Http\Middleware\HelloMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/hello/{id}', [HelloController::class, 'index'])->where('id', '[0-9]+');
+Route::get('/hello/{person}', [HelloController::class, 'index']);
+
+Route::middleware([HelloMiddleware::class])->group(function () {
+    Route::get('/hello', [HelloController::class, 'index'])->name('hello');
+    // Route::get('/hello/other', [HelloController::class, 'other']);
 });
